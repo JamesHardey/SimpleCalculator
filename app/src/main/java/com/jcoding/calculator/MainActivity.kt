3baa3b9a16d3998550d8219d4f066481d984d8ab
JamesHardey <jamesade646@gmail.com>
2022-10-13 09:40:53 -0400

    update

---
+++ app/src/main/java/com/jcoding/calculator/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.jcoding.calculator
+
+import android.os.Bundle
+import android.view.WindowManager
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.jcoding.calculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.apply {
+            setFlags( WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
+
+    }
+
+}
